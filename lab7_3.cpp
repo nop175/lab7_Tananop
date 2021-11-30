9ccece0b7a4d13eb829507f3ecb4bdc036348222
nop175 <tananop_them@gmail.com>
2021-11-30 17:12:35 +0700

    Q3,4,5

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,36 @@
+Pkg Ton
 #include<iostream>
 
 using namespace std;
 
+double adiff(int x,int y){
+  if(x>=360 or x <= -360){
+    x = x % 360;
+  }
+  if(y>=360 or y <= -360){
+    y = y % 360;
+  }
+  int z;
+  if(x>=y){
+    z = x-y;
+  }else{
+    z = y-x;
+  }
+  if(z>180){
+    z = 360 - z; 
+  }
+
+  return z ;
+}
+
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
   cout << adiff(60,244);
 }
