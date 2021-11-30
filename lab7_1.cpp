5be12dca9ce6bc8c49c91692233b7b951e2ac409
nop175 <tananop_them@gmail.com>
2021-11-30 16:03:59 +0700

    Question2

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,39 +3,54 @@
 
 using namespace std;
 
-string func1(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += x[L-i-1];
-		i++;
-	}
-	return y;
+string func1(string x)
+{
+    int i = 0, L = x.size();
+    string y = "";
+    while(i < L)
+    {
+        y+= x[L-i-1];
+        i++;
+    }
+    return y;
 }
 
-string func2(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += toupper(x[i]);
-		i++;
-	}
-	return y;	
+string func2(string x)
+{
+    int i = 0, L = x.size();
+    string y = "";
+    while(i < L)
+    {
+        y+= toupper(x[i]);
+        i++;
+    }
+    return y;
 }
 
-string func3(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += tolower(x[i]);
-		i++;
-	}
-	return y;	
+string func3(string x)
+{
+    int i = 0, L = x.size();
+    string y = "";
+    while(i < L)
+    {
+        y+= tolower(x[i]);
+        i++;
+    }
+    return y;
 }
 
-int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+int main()
+{
+    string a;
+    cout << "Input text: ";
+    cin >> a;
+    cout << "Reversed text: " << func1(a) << "\n";
+    cout << "Palindrome: ";
+    if(func2(a) == func2(func1(a))){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
     return 0;
 }
